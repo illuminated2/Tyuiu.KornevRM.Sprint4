b0add7043528551a940de6bc0a21733bff452084
Руслан Корнев <iLLuminated_89@mail.ru>
2024-10-26 19:32:05 +0500

    Add project files.

---
+++ Tyuiu.KornevRM.Sprint4.Task0.V15.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.KornevRM.Sprint4.Task0.V15.Lib
+{
+    public class DataService : ISprint4Task0V15
+    {
+        
+        public int GetMultOddArrEl(int[] array)
+        {
+            int res = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] %2 == 0)
+                {
+
+                }
+                else
+                {
+                    res *= array[i];
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KornevRM.Sprint4.Task0.V15/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.KornevRM.Sprint4.Task0.V15.Lib;
+namespace Tyuiu.KornevRM.Sprint4.Task0.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("*ИСКХОДНЫЕ ДАННЫЕ:                                                    *");
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("Исходный массив:");
+            int[] array = { 9, 8, 7, 6, 5, 7, 3, 2, 7, 3 };
+            for (int i = 0; i < array.Length; i++) { 
+            Console.WriteLine(array[i]+" ");
+            }
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("***********************************************************************");
+            int p = Convert.ToInt32(ds.GetMultOddArrEl(array));
+            Console.WriteLine(p);
+            Console.ReadKey();
+
+        }
+    }
+}
