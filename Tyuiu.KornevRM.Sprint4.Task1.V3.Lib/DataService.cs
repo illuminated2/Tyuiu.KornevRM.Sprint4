c58b236ee68ba29cbb5ccf40f3e1fa4212e7031e
Руслан Корнев <iLLuminated_89@mail.ru>
2024-10-26 20:06:44 +0500

    Task1

--- Tyuiu.KornevRM.Sprint4.Task1.V3.Lib/DataService.cs
+++ Tyuiu.KornevRM.Sprint4.Task1.V3.Lib/DataService.cs
@@ -8,16 +8,14 @@ namespace Tyuiu.KornevRM.Sprint4.Task1.V3.Lib
             int r = 1;
             for (int i = 0; i < array.Length; i++)
             {
-                if(i >= 2 && i <= 9)
-                {
+               
                     if(array[i] % 2 == 0)
                     {
                         r *= array[i];
                     }
-                }
+                
             }
             return r;
-
         }
     }
 }
